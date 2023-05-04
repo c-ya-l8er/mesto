9e54f3bad312518777c41587f290632a103d2009
c-ya-l8er <c-ya-l8er@ya.ru>
2023-05-05 01:26:40 +0300

    подправил внешний вид и доделал JS

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
-document.getElementById('open-popup-btn').addEventListener('click', function(){
-    document.getElementById('edit-form').classList.add('popup_opened')
-})
+const popup = document.querySelector(".popup");
+const openPopupButton = document.querySelector(".profile__edit-btn");
+const closePopupButton = document.querySelector(".popup__close-btn");
+const togglePopupState = (popupToToggle) =>
+  popupToToggle.classList.toggle("popup_opened");
+const formElement = document.querySelector(".popup__form");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+const nameInput = document.querySelector(".popup__field_type_name");
+const jobInput = document.querySelector(".popup__field_type_about");
 
-document.getElementById('close-popup-btn').addEventListener('click', function(){
-    document.getElementById('edit-form').classList.remove('popup_opened')
-})
-
-document.querySelector('#edit-form .popup__container').addEventListener('click', event => {
-    event.isClickWithinPopup = true;
+openPopupButton.addEventListener("click", popupOpenHandler);
+closePopupButton.addEventListener("click", () => togglePopupState(popup));
+popup.addEventListener("click", (evt) => {
+  if (evt.target === popup) togglePopupState(popup);
 });
 
-document.getElementById('edit-form').addEventListener('click', event => {
-    if (event.isClickWithinPopup) return;
-    event.currentTarget.classList.remove('popup_opened')
-});
+function popupOpenHandler() {
+  togglePopupState(popup);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  togglePopupState(popup);
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = jobInput.value;
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
